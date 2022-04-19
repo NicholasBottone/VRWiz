cab13de74bd7456f76f8fa652cd1502211d87021
Nicholas Bottone <nick@bottone.io>
2022-04-18 23:50:41 -0400

    Create Vite react app

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 import mkcert from "vite-plugin-mkcert";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   server: { https: true },
-  plugins: [mkcert()],
+  plugins: [react(), mkcert()],
 });
